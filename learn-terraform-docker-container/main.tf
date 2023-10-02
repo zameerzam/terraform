0c902460bcb6772f5f79922ef6cadc354e94af64
zameerzam <zameer.ullah@hotmail.com>
2023-10-02 08:31:12 -0500

    terraform version changes

--- learn-terraform-docker-container/main.tf
+++ learn-terraform-docker-container/main.tf
@@ -6,17 +6,17 @@ terraform {
     # }
     aws = {
       source = "hashicorp/aws"
-      version = "-> 3.27"
+      version = "~> 4.16"
     }
   }
 }
 
 provider "aws" {
   profile = "default"
-  region = "us-west-2"
+  region = "us-east-2"
 }
 resource "aws_instance" "app_server" {
-  ami = "ami-83c94e3"
+  ami = "ami-05803413c51f242b7"
   instance_type = "t2.micro"
   tags = {
     Name = "ZameerApp"
@@ -24,19 +24,3 @@ resource "aws_instance" "app_server" {
   
 }
 
-provider "docker" {}
-
-resource "docker_image" "nginx" {
-  name         = "nginx"
-  keep_locally = false
-}
-
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.image_id
-  name  = "tutorial"
-
-  ports {
-    internal = 80
-    external = 8000
-  }
-}
